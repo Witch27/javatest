4e3f910967af454531d945794b076bb012f34f64
Witch27 <109660713+Witch27@users.noreply.github.com>
2022-07-25 10:31:04 +0200

    javatest
    javatest

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person vitya = new Person(120, "Vitya");
+        vitya.say("George");
+        System.out.println(vitya.name +" height is "+vitya.height);
+        Person vlad = new Person();
+        System.out.println(vlad.height);
+
+        Person dima = new Person("Dima");
+        System.out.println(dima.name);
+        System.out.println("");
+        Student freshman = new Student(76, "Viktor", 2);
+        freshman.tell();
+        Student fresh = new Student(280, "Vlad", 4);
+        fresh.tell();
+    }
+}
---
+++ src/com/company/Person.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Person {
+    int height = 180;
+    public  String name = "Default";
+
+    public void say(String name){
+        System.out.println("Hello, "+name);
+    }
+    public  Person(){
+    }
+
+    public  Person(int h, String n){
+        height = h;
+        name = n;
+
+    }
+    public  Person (String n){
+        name = n;
+    }
+
+}
